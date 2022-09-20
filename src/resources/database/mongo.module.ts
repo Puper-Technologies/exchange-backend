cf5d1a962dbfa0369e0e1f3b6403c106796a9a79
gosrahul21 <gosrahul21@gmail.com>
2022-09-20 21:08:58 +0530

    some changes

---
+++ src/resources/database/mongo.module.ts
@@ -0,0 +1,36 @@
+import { Module, DynamicModule, InternalServerErrorException } from "@nestjs/common";
+import { ConfigModule } from "../../config/config.module";
+import { ConfigService } from "../../config/config.service";
+import { MongooseModule, MongooseModuleOptions } from "@nestjs/mongoose";
+@Module({})
+export class MongoModule {
+
+  public static getNoSqlConnectionOptions(config: ConfigService): MongooseModuleOptions {
+    const dbdata = config.get().database;
+    if (!dbdata) {
+      throw new InternalServerErrorException('Database config is missing..!');
+    }
+
+    let { host, port, dbName, userName, password } = dbdata;
+
+    return {
+      uri:'mongodb://localhost:27017/'+dbName
+      // uri: `mongodb://${userName}:${password}@${host}:${port}/${dbName}?authSource=admin&readPreference=primary`
+    };
+  }
+  public static forRoot(): DynamicModule {
+    return {
+      module: MongoModule,
+      imports: [
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => MongoModule.getNoSqlConnectionOptions(configService),
+          inject: [ConfigService],
+        }),
+      ],
+      controllers: [],
+      providers: [],
+      exports: [],
+    };
+  }
+}
